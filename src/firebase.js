6a35867f33646d846ba591fe37235ca1c0b1b546
Alex Knott <alextknott@gmail.com>
2022-04-29 16:13:37 +0100

    Integrate firebase to project and initialize it

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDeBoufiCMWmsqzYePnvjr-F7Z1FJFpFxg",
+  authDomain: "wheres-wally-28592.firebaseapp.com",
+  projectId: "wheres-wally-28592",
+  storageBucket: "wheres-wally-28592.appspot.com",
+  messagingSenderId: "189351626319",
+  appId: "1:189351626319:web:cd79cabb945f3555070c3a",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
