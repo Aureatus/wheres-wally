01e37358f83f47029b5bab6ffb9ddd37b8916d5d
Alex Knott <alextknott@gmail.com>
2022-05-01 16:14:31 +0100

    Implement drawing of wheres wally image onto canvas

--- src/Components/GameCanvas.jsx
+++ src/Components/GameCanvas.jsx
@@ -1,5 +1,21 @@
+import { useEffect, useState } from "react";
+import wallyImage1 from "../Assets/wheres-wally-1.jpg";
+
 const GameCanvas = () => {
-  return <canvas id="canvas"></canvas>;
+  const [imageHeight, setImageHeight] = useState(0);
+  const [imageWidth, setImageWidth] = useState(0);
+
+  useEffect(() => {
+    const context = document.querySelector("#canvas").getContext("2d");
+    const img = new Image();
+    img.src = wallyImage1;
+    img.onload = () => {
+      setImageHeight(img.height);
+      setImageWidth(img.width);
+      context.drawImage(img, 0, 0);
+    };
+  });
+  return <canvas id="canvas" height={imageHeight} width={imageWidth}></canvas>;
 };
 
 export default GameCanvas;
