3e58effc69714ad285bffefae376e11b45ca5ff8
Alex Knott <alextknott@gmail.com>
2022-05-01 16:05:42 +0100

    Change firebaseConfig to a new project

--- src/Components/Firebase.jsx
+++ src/Components/Firebase.jsx
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDeBoufiCMWmsqzYePnvjr-F7Z1FJFpFxg",
-  authDomain: "wheres-wally-28592.firebaseapp.com",
-  projectId: "wheres-wally-28592",
-  storageBucket: "wheres-wally-28592.appspot.com",
-  messagingSenderId: "189351626319",
-  appId: "1:189351626319:web:cd79cabb945f3555070c3a",
+  apiKey: "AIzaSyARq8iPDaAvq2ZLrYYYyVsw146hbLbpFvg",
+  authDomain: "wheres-wally-1fa55.firebaseapp.com",
+  projectId: "wheres-wally-1fa55",
+  storageBucket: "wheres-wally-1fa55.appspot.com",
+  messagingSenderId: "907449387792",
+  appId: "1:907449387792:web:01b104d7f66e4c38363395",
 };
 
 const app = initializeApp(firebaseConfig);
