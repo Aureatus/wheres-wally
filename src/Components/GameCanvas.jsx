4cdb1f28af19f32432f7a734644cb03a2b6de5a2
Alex Knott <alextknott@gmail.com>
2022-05-01 18:12:01 +0100

    Simplified rendering method for GameCanvas

--- src/Components/GameCanvas.jsx
+++ src/Components/GameCanvas.jsx
@@ -2,20 +2,16 @@ import { useEffect, useState } from "react";
 import wallyImage1 from "../Assets/wheres-wally-1.jpg";
 
 const GameCanvas = () => {
-  const [imageHeight, setImageHeight] = useState(0);
-  const [imageWidth, setImageWidth] = useState(0);
-
   useEffect(() => {
     const context = document.querySelector("#canvas").getContext("2d");
-    const img = new Image();
-    img.src = wallyImage1;
-    img.onload = () => {
-      setImageHeight(img.height);
-      setImageWidth(img.width);
-      context.drawImage(img, 0, 0);
+
+    const image1 = new Image();
+    image1.src = wallyImage1;
+    image1.onload = () => {
+      context.drawImage(image1, 0, 0);
     };
-  });
-  return <canvas id="canvas" height={imageHeight} width={imageWidth}></canvas>;
+  }, []);
+  return <canvas id="canvas" height={1136} width={640}></canvas>;
 };
 
 export default GameCanvas;
