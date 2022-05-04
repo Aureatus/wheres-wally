fd82a681bdc007d5578b04dce7f0570cfc961aaa
Alex Knott <alextknott@gmail.com>
2022-05-04 15:42:37 +0100

    Implement targeting box
    Targeting box is drawn on click at mouse cursor, and is removed upon
    clicking again. A div sibling to canvas is conditionally rendered when
    targeting box is present.

--- src/Components/GameCanvas.jsx
+++ src/Components/GameCanvas.jsx
@@ -1,19 +1,77 @@
-import { useEffect, useRef } from "react";
-import wallyImage1 from "../Assets/wheres-wally-1.jpg";
+import { useEffect, useRef, useState } from "react";
 
-const GameCanvas = ({ drawWallyImage }) => {
+const GameCanvas = ({ drawWallyImage, wallyImage }) => {
   const canvasRef = useRef(null);
+
+  const [mouseClickLocation, setMouseClickLocation] = useState(null);
+
+  const getMouseCoordinates = (event) => {
+    const mouseX = event.nativeEvent.offsetX;
+    const mouseY = event.nativeEvent.offsetY;
+    return { mouseX, mouseY };
+  };
+
+  const [targetingBoxPresent, setTargetingBoxPresent] = useState(false);
+  const boxWidth = 50;
+  const boxHeight = 50;
+
+  const drawTargetBox = ({ mouseClickLocation }, context) => {
+    const { mouseX, mouseY } = mouseClickLocation;
+
+    context.strokeRect(
+      mouseX - boxWidth / 2,
+      mouseY - boxHeight / 2,
+      boxWidth,
+      boxHeight
+    );
+  };
 
   useEffect(() => {
     const context = canvasRef.current.getContext("2d");
-
-    const image1 = new Image();
-    image1.src = wallyImage1;
-    image1.onload = () => {
-      drawWallyImage(context, image1);
+    wallyImage.onload = () => {
+      drawWallyImage(context, wallyImage);
     };
-  }, [drawWallyImage]);
-  return <canvas ref={canvasRef} height={1136} width={640}></canvas>;
+  }, [drawWallyImage, wallyImage]);
+
+  useEffect(() => {
+    const context = canvasRef.current.getContext("2d");
+    drawWallyImage(context, wallyImage);
+  });
+
+  useEffect(() => {
+    if (mouseClickLocation) {
+      const context = canvasRef.current.getContext("2d");
+      drawTargetBox({ mouseClickLocation }, context);
+    }
+  }, [mouseClickLocation]);
+
+  if (!targetingBoxPresent) {
+    return (
+      <canvas
+        ref={canvasRef}
+        height={1136}
+        width={640}
+        onClick={(event) => {
+          setMouseClickLocation(getMouseCoordinates(event));
+          setTargetingBoxPresent(true);
+        }}
+      ></canvas>
+    );
+  } else {
+    return (
+      <>
+        <div>a</div>
+        <canvas
+          ref={canvasRef}
+          height={1136}
+          width={640}
+          onClick={() => {
+            setTargetingBoxPresent(false);
+          }}
+        ></canvas>
+      </>
+    );
+  }
 };
 
 export default GameCanvas;
