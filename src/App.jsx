ebd0ae865ca560f5c1c0416e1aa37b4343a9111d
Alex Knott <alextknott@gmail.com>
2022-05-04 15:37:13 +0100

    Import wally image, create new image element using it and pass it as prop to GameCanvas

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import GameCanvas from "./Components/GameCanvas";
+import wallyImage1 from "./Assets/wheres-wally-1.jpg";
 
 function App() {
   const drawWallyImage = (context, image1) => {
     context.drawImage(image1, 0, 0);
   };
+  const wallyImage = new Image();
+  wallyImage.src = wallyImage1;
+
   return (
     <div className="App">
       <header></header>
       <main>
-        <GameCanvas drawWallyImage={drawWallyImage} />
+        <GameCanvas drawWallyImage={drawWallyImage} wallyImage={wallyImage} />
       </main>
     </div>
   );
