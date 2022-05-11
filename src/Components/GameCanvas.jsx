8e4180fa1de8474f8a2a73dc0a5b5b4ad347167e
Alex Knott <alextknott@gmail.com>
2022-05-11 14:31:58 +0100

    Render character selection div with each character contained inside a seperate div inside

--- src/Components/GameCanvas.jsx
+++ src/Components/GameCanvas.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useRef, useState } from "react";
 
 import wallyProfile from "../Assets/wally.jpg";
+import odlawProfile from "../Assets/Odlaw.jpg";
+import wizardProfile from "../Assets/Wizard.jpeg";
 
 const GameCanvas = ({ drawWallyImage, wallyImage1 }) => {
   const canvasRef = useRef(null);
@@ -62,6 +64,27 @@ const GameCanvas = ({ drawWallyImage, wallyImage1 }) => {
   } else {
     return (
       <>
+        <div
+          className="char-select-div"
+          style={{
+            position: "absolute",
+            left: mouseClickLocation.mouseX - boxWidth / 2 + 50,
+            top: mouseClickLocation.mouseY - boxHeight / 2,
+          }}
+        >
+          <div className="char-select-card" id="wally">
+            <img src={wallyProfile} alt="Wally" height={26} width={26} />
+            <p>Wally</p>
+          </div>
+          <div className="char-select-card" id="waldo">
+            <img src={odlawProfile} alt="Odlaw" height={26} width={26} />
+            <p>Wally</p>
+          </div>
+          <div className="char-select-card" id="wizard">
+            <img src={wizardProfile} alt="Wizard" height={26} width={26} />
+            <p>Wally</p>
+          </div>
+        </div>
         <canvas
           ref={canvasRef}
           height={1075}
