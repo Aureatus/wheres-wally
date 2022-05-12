ec2b57f32f7a800e119b45203334aefcf79384b3
Alex Knott <alextknott@gmail.com>
2022-05-12 18:01:37 +0100

    Create function that checks if character selected is inside of the targeting box and returns true or false

--- src/Components/CharacterSelect.jsx
+++ src/Components/CharacterSelect.jsx
@@ -9,7 +9,32 @@ const CharacterSelect = ({
   odlawProfile,
   wizardProfile,
 }) => {
-  const characterCoordinate = useContext(characterCoordinateContext);
+  const characterCoordinates = useContext(characterCoordinateContext);
+
+  const checkIfCharacterAtMouseCoords = (character) => {
+    const objectKey = character + "Location";
+    const selectedCharacterCoordinates = characterCoordinates[objectKey];
+    let xCoordsCorrect = false;
+    let yCoordsCorrect = false;
+
+    if (
+      mouseClickLocation.mouseX >= selectedCharacterCoordinates.x - 25 &&
+      mouseClickLocation.mouseX <= selectedCharacterCoordinates.x + 25
+    ) {
+      xCoordsCorrect = true;
+    }
+
+    if (
+      mouseClickLocation.mouseY >= selectedCharacterCoordinates.y - 25 &&
+      mouseClickLocation.mouseY <= selectedCharacterCoordinates.y + 25
+    ) {
+      yCoordsCorrect = true;
+    }
+
+    if (xCoordsCorrect && yCoordsCorrect) {
+      return true;
+    } else return false;
+  };
 
   return (
     <div
@@ -20,15 +45,27 @@ const CharacterSelect = ({
         top: mouseClickLocation.mouseY - boxHeight / 2 - 50,
       }}
     >
-      <div className="char-select-card" id="wally">
+      <div
+        className="char-select-card"
+        id="wally"
+        onClick={() => checkIfCharacterAtMouseCoords("Wally")}
+      >
         <img src={wallyProfile} alt="Wally" height={26} width={26} />
         <p>Wally</p>
       </div>
-      <div className="char-select-card" id="waldo">
+      <div
+        className="char-select-card"
+        id="waldo"
+        onClick={() => checkIfCharacterAtMouseCoords("Odlaw")}
+      >
         <img src={odlawProfile} alt="Odlaw" height={26} width={26} />
         <p>Odlaw</p>
       </div>
-      <div className="char-select-card" id="wizard">
+      <div
+        className="char-select-card"
+        id="wizard"
+        onClick={() => checkIfCharacterAtMouseCoords("Wizard")}
+      >
         <img src={wizardProfile} alt="Wizard" height={26} width={26} />
         <p>Wizard</p>
       </div>
